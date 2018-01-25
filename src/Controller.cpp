5bbc7404b1104250d4737256ed82a2ea5a3f2ff3
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-25 13:46:51 -0700

    Added code to mess around with arrays.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -20,6 +20,9 @@ void Controller :: start()
 {
 	string name = "Jeremy Saysanasy";
 	string * nameptr = &name;
+	int evenInt [5] = { 2, 4, 6, 8, 10};
+	double numbersOfDouble [5] = { 2.2, 4.4, 6.6, 8.8, 10.10};
+	string names [5] = {"Jem", "Jack", "Parsley", "Pandama", "Cerci"};
 
 	for (int index = 0; index <= 4; index++)
 	{
@@ -31,6 +34,21 @@ void Controller :: start()
 	for (int index = 13; index <= 31; index++)
 	{
 		cout << index << endl;
+	}
+
+	for (int count : evenInt)
+	{
+		cout << "This array has " << count << " as a even number." << endl;
+	}
+
+	for (double count : numbersOfDouble)
+	{
+		cout << "Decimals are weird looking." << count << endl;
+	}
+
+	for (string count : names)
+	{
+		cout << "These are names that I randomly found: " << count << endl;
 	}
 
 }
