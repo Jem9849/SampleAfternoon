8ddadca1bb0e7a0cab96e1f63930add7ee5d3c41
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-25 12:04:11 -0700

    I added several files so far, and I made a project.

---
+++ src/Controller.hpp
@@ -0,0 +1,20 @@
+//
+// Controller.hpp
+// SampleAfternoon
+//
+//
+//
+//
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+class Controller
+{
+private:
+	// Just like Java
+	// data members and internal methods
+protected:
+
+};
---
+++ src/Runner.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : SampleAfternoon.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include "Controller.hpp"
+
+int main() {
+	Controller app;
+	app.start();
+}
